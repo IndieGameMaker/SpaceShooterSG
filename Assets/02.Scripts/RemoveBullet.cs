0fc9624dcf46aa0cd708307bd2c1bb5eb6a5027f
Lee JaeHyun <myleje@gmail.com>
2020-11-08 09:27:04 +0900

    .

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RemoveBullet : MonoBehaviour
 {
     //충돌 시점에 1번 호출
+    //Collider 컴포넌트의 Is Trigger 언체크
     void OnCollisionEnter(Collision coll)
     {
         if (coll.gameObject.tag == "BULLET")
@@ -21,5 +22,14 @@ public class RemoveBullet : MonoBehaviour
     //두 물체가 떨어졌을 경우에 1번 호출
     void OnCollisionExit(Collision coll)
     {}
+*/
+
+
+/*
+Collider Is Trigger 체크 : 관통하는 성격
+OnTriggerEnter
+OnTriggerStay
+OnTriggerExit
+
 */
 }
