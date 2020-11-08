ee20aa0e842582efb14749744840f2ea731df13c
Lee JaeHyun <myleje@gmail.com>
2020-11-08 09:23:42 +0900

    .

---
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    //충돌 시점에 1번 호출
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "BULLET")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+
+/*
+    //충돌을 하고 있는 동안 계속 호출
+    void OnCollisionStay(Collision coll)
+    {}
+
+    //두 물체가 떨어졌을 경우에 1번 호출
+    void OnCollisionExit(Collision coll)
+    {}
+*/
+}
