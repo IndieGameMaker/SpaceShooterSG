d29e9ee5061b56c80234a6911a005611e440aee0
Lee JaeHyun <myleje@gmail.com>
2020-11-08 11:13:56 +0900

    .

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -23,6 +23,11 @@ public class BarrelCtrl : MonoBehaviour
         //AddComponent
         Rigidbody rb = this.gameObject.AddComponent<Rigidbody>();
         rb.AddForce(Vector3.up * 1500.0f);
+        Destroy(this.gameObject, 2.0f);
+
+        //폭발효과
+        Quaternion rot = Quaternion.Euler(0, Random.Range(0, 360), 0);
+        Instantiate(expEffect, this.transform.position, rot);
     }
 
 }
