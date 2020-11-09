b81038c66417a97448d04e3c9d25b82686033e42
Lee JaeHyun <myleje@gmail.com>
2020-11-09 12:37:18 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -6,17 +6,22 @@ public class GameManager : MonoBehaviour
 {
     public GameObject monsterPrefab;
     public Transform[] points;
+    public float createTime = 3.0f;
     
     void Start()
     {
         points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();
         monsterPrefab = Resources.Load<GameObject>("monster");
         //monsterPrefab = Resources.Load("monster") as GameObject;
+        InvokeRepeating("CreateMonster", 1.0f, createTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateMonster()
     {
-        
+        int idx = UnityEngine.Random.Range(1, points.Length);
+        GameObject monster = Instantiate<GameObject>(monsterPrefab);
+        monster.name = "Monster";
+        monster.transform.position = points[idx].position;
+        monster.transform.rotation = Quaternion.LookRotation(points[0].position - points[idx].position);
     }
 }
