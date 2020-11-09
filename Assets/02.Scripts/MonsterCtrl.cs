16ef14d4e2edf7d6b2f6b4336af8bbebe42f689a
Lee JaeHyun <myleje@gmail.com>
2020-11-09 09:16:26 +0900

    .

---
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCtrl : MonoBehaviour
+{
+    public Transform playerTr;
+
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("PLAYER");
+        if (player != null)
+        {
+            playerTr = player.GetComponent<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
