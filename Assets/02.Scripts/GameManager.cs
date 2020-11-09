834590be68519316281a9f8b087ca78178c38f15
Lee JaeHyun <myleje@gmail.com>
2020-11-09 12:43:26 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -7,21 +7,30 @@ public class GameManager : MonoBehaviour
     public GameObject monsterPrefab;
     public Transform[] points;
     public float createTime = 3.0f;
+
+    public bool isGameOver = false;
     
     void Start()
     {
         points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();
         monsterPrefab = Resources.Load<GameObject>("monster");
         //monsterPrefab = Resources.Load("monster") as GameObject;
-        InvokeRepeating("CreateMonster", 1.0f, createTime);
+        //InvokeRepeating("CreateMonster", 1.0f, createTime);
+        StartCoroutine(CreateMonster());
     }
 
-    void CreateMonster()
+    IEnumerator CreateMonster()
     {
-        int idx = UnityEngine.Random.Range(1, points.Length);
-        GameObject monster = Instantiate<GameObject>(monsterPrefab);
-        monster.name = "Monster";
-        monster.transform.position = points[idx].position;
-        monster.transform.rotation = Quaternion.LookRotation(points[0].position - points[idx].position);
+        yield return new WaitForSeconds(1.0f);
+
+        while(!isGameOver)
+        {
+            int idx = UnityEngine.Random.Range(1, points.Length);
+            GameObject monster = Instantiate<GameObject>(monsterPrefab);
+            monster.name = "Monster";
+            monster.transform.position = points[idx].position;
+            monster.transform.rotation = Quaternion.LookRotation(points[0].position - points[idx].position);
+            yield return new WaitForSeconds(createTime);
+        }
     }
 }
